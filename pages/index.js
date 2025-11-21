85b65e0f08ba1ba915498226bdb3c097b9c85345
osmar570 <osmar570@outlook.com>
2025-11-21 00:35:34 -0300

    fix: update text color in Home component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
     return (
-        <h1 style={{ color: "rgba(202, 3, 252, 1)" }}>Sabrine, amo você, se você me ama, dá uma risadinha</h1>
+        <h1 style={{ color: "rgba(252, 3, 3, 1)" }}>Sabrine, amo você, se você me ama, dá uma risadinha</h1>
     );
 }
 
