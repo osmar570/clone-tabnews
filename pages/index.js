75062e586618320a4bb94b47ecd4b31458214f59
osmar570 <osmar570@outlook.com>
2025-11-21 00:40:42 -0300

    fix: add global style for body margin in Home component

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,25 @@
 function Home() {
   return (
-    <div
-      style={{
-        backgroundColor: "black",
-        color: "pink",
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <h1>Sabrine, amo você, se você me ama, dá uma risadinha</h1>
-    </div>
+    <>
+      <style jsx global>{`
+        body {
+          margin: 0;
+        }
+      `}</style>
+      <div
+        style={{
+          backgroundColor: "black",
+          color: "pink",
+          height: "100vh",
+          width: "100vw",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <h1>Sabrine, amo você, se você me ama, dá uma risadinha</h1>
+      </div>
+    </>
   );
 }
 
