cac54d00623cde91c548f87c7ec137513155f45b
osmar570 <osmar570@outlook.com>
2025-11-23 13:36:15 -0300

    refactor: update status API response format and improve test description

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,9 @@
 function status(req, res) {
-  let retorno = { status: "OK", message: "API is running" };
-  res.status(200).json({ data: retorno });
+  let retorno = {
+    status: "OK",
+    message: "API is running",
+  };
+  res.status(200).json({ lastUpdate: new Date().toISOString(), data: retorno });
 }
 
 export default status;
