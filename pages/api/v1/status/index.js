1c79a9f9384fc843dbe9c7c17be2a2d5326637ef
osmar570 <osmar570@outlook.com>
2025-11-23 16:11:18 -0300

    feat: implement PostgreSQL database connection and update API status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,12 @@
-function status(req, res) {
+import database from "../../../../infra/database.js";
+
+async function status(req, res) {
   let retorno = {
     status: "OK",
     message: "API is running",
   };
+  const result = await database.query("SELECT 1+1 as Soma;");
+  console.log(result.rows);
   res.status(200).json({ lastUpdate: new Date().toISOString(), data: retorno });
 }
 
